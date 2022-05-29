6c5daca8cc4d63fc37bc6cf17564836e6791a8e3
venkikgit <venki.apiiit@gmail.com>
2022-05-29 20:34:36 +0530

    pre-final

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
+//Below line for accessing input input information
 const input = document.getElementById('input-info-val');
+//Below line for accessing add task button information
 const addbtn = document.getElementById('input-info-btn');
+//Below line to make changes based on add button click
 addbtn.addEventListener('click',function(){
+    //If statement checks if there exists only space inputs or unncessary spaces in the string in both ends and trims the spaces
      if(input.value.trim()!=0){
+       //Create localItems to get local storage object
     let localItems = JSON.parse(localStorage.getItem('Todo'));
+    //if local items contains no elements then we will create list array 
+    //and set  the local storage information into list array
     if(localItems ===null){
         listarray =[]
     }else{
         listarray = localItems;
     }
+    //To push enetered task info into list array
     listarray.push(input.value);
+    //To store in the local storage in key value pairs
     localStorage.setItem('Todo',JSON.stringify(listarray));
     }
+    //Function calling to display task info on the webpage
     showlist();
 });
 
+//Function to display task information
 function  showlist() {
     let output ='';
     let showList = document.querySelector('.task-list');
@@ -40,6 +51,7 @@ function  showlist() {
   }
 
   showlist();
+  //Function to delete the task
 
   function deleteTask(index) {  
     let localItems = JSON.parse(localStorage.getItem('Todo'));
@@ -48,11 +60,13 @@ function  showlist() {
     showlist();
 
   }
+  //Function to clear tasks
   function clearAll () { 
       localStorage.clear();
       showlist();
    }
 
+   //Below block for changing the finished task attributes to look different
    const allInputs = document.querySelectorAll("input");
    allInputs.forEach(input =>{
        input.addEventListener('click',ele=>{
